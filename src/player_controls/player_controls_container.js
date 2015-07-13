c6cd1bfddc60995148c99bdc0f7deb69db94256a
Emil Löfquist <lofquist@gmail.com>
2015-07-13 16:45:11 +0200

    Hookup controls

--- src/player_controls/player_controls_container.js
+++ src/player_controls/player_controls_container.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ButtonGroup, Button } from 'react-bootstrap';
 import classNames from 'classnames';
+import FirebaseRef from 'firebase_ref';
+import _ from 'lodash';
 
 class PlayerControlsContainer extends React.Component {
 
@@ -13,7 +15,26 @@ class PlayerControlsContainer extends React.Component {
   }
 
   togglePlay() {
-    this.setState({ playing: !this.state.playing });
+    FirebaseRef.child('player/playing').set(!this.state.playing);
+  }
+
+  playNext() {
+    FirebaseRef.child('player/next').set(true);
+  }
+
+  onPlayerChange(snapshot) {
+    let val = snapshot.val();
+    if(!_.isNull(val)) {
+      this.setState({ playing: val });
+    }
+  }
+
+  componentDidMount() {
+    FirebaseRef.child('player/playing').on('value', this.onPlayerChange.bind(this));
+  }
+
+  componentWillUnmount() {
+    FirebaseRef.child('player/playing').off('value', this.onPlayerChange.bind(this));
   }
 
   render() {
@@ -24,7 +45,7 @@ class PlayerControlsContainer extends React.Component {
       <div>
         <ButtonGroup>
           <Button onClick={this.togglePlay.bind(this)}><i className={playPauseKlass} /></Button>
-          <Button><i className="fa fa-step-forward" /></Button>
+          <Button onClick={this.playNext.bind(this)}><i className="fa fa-step-forward" /></Button>
         </ButtonGroup>
         <Button bsStyle="link"><i className="fa fa-random" /></Button>
       </div>
