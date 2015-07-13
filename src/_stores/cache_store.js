afdb253f66613245970fbd75345cb2af88aff91d
Emil Löfquist <lofquist@gmail.com>
2015-07-13 16:43:44 +0200

    Cache store

---
+++ src/_stores/cache_store.js
@@ -0,0 +1,26 @@
+import Reflux from 'reflux';
+import _ from 'lodash';
+
+let CacheStore = Reflux.createStore({
+
+  init() {
+    this.cache = [];
+  },
+
+  getStore() {
+    return this.cache;
+  },
+
+  get(cacheKey) {
+    return (_.filter(this.cache, (item) => {
+      return item.key === cacheKey;
+    }))[0];
+  },
+
+  set(key, data) {
+    this.cache.push({ key: key, data: data });
+  }
+
+});
+
+export default CacheStore;
