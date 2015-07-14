f14170c754b88ac05dc92630c0cda9633a05cfb7
Emil Löfquist <lofquist@gmail.com>
2015-07-15 01:41:46 +0200

    Shuffle button

---
+++ src/player_controls/shuffle_button.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import FirebaseRef from 'firebase_ref';
+import { Button } from 'react-bootstrap';
+
+class ShuffleButton extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isShuffle: false
+    };
+  }
+
+  onShuffleChange(snapshot) {
+    let val = snapshot.val();
+    console.log(val);
+    if(!_.isNull(val)) {
+      this.setState({ isShuffle: val });
+    }
+  }
+
+  handleClick() {
+    FirebaseRef.child('playlist/shuffle').set(!this.state.isShuffle);
+  }
+
+  componentDidMount() {
+    FirebaseRef.child('playlist/shuffle').on('value', this.onShuffleChange.bind(this));
+  }
+
+  componentWillUnmount() {
+    FirebaseRef.child('playlist/shuffle').off('value', this.onShuffleChange.bind(this));
+  }
+
+  render() {
+    return (
+      <Button onClick={this.handleClick.bind(this)} bsStyle="link"><i className="fa fa-random" /></Button>
+    );
+  }
+};
+
+export default ShuffleButton;
