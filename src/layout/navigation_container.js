05bc295fb216ec59c3610faca97b0d1fa6106488
Emil Löfquist <lofquist@gmail.com>
2015-07-29 08:58:08 +0200

    Nest search button in nav

--- src/layout/navigation_container.js
+++ src/layout/navigation_container.js
@@ -34,27 +34,25 @@ class NavigationContainer extends React.Component {
 
   render() {
     return (
-      <div>
+      <Nav onClick={this.props.hideSearchContainer} stacked bsStyle="pills">
         <Button bsStyle="link" onClick={this.props.toggleSearch}>
           <i className="fa fa-search" />
           <span className="sr-only">Search</span>
         </Button>
-        <Nav onClick={this.props.hideSearchContainer} stacked bsStyle="pills">
-          <NavItemLink to='playlist'>
-            <i className="fa fa-headphones" />
-            <span className="sr-only">Playlist</span>
-          </NavItemLink>
-          <NavItemLink to='queue'>
-            <i className="fa fa-bars" />
-            <span className="sr-only">Queue</span>
-            <Badge title="Songs in queue" className="in-queue">{this.state.queueSize}</Badge>
-          </NavItemLink>
-          <NavItemLink to='starred' className="hide">
-            <span className="sr-only">Starred</span>
-            <i className="fa fa-star" />
-          </NavItemLink>
-        </Nav>
-      </div>
+        <NavItemLink to='playlist'>
+          <i className="fa fa-headphones" />
+          <span className="sr-only">Playlist</span>
+        </NavItemLink>
+        <NavItemLink to='queue'>
+          <i className="fa fa-bars" />
+          <span className="sr-only">Queue</span>
+          <Badge title="Songs in queue" className="in-queue">{this.state.queueSize}</Badge>
+        </NavItemLink>
+        <NavItemLink to='starred' className="hide">
+          <span className="sr-only">Starred</span>
+          <i className="fa fa-star" />
+        </NavItemLink>
+      </Nav>
     );
   }
 };
