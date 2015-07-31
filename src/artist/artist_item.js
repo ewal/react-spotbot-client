25ca466cb70b20da27753aaf5df412a6bd43adfe
Emil Löfquist <lofquist@gmail.com>
2015-07-31 12:55:41 +0200

    Use background image module

--- src/artist/artist_item.js
+++ src/artist/artist_item.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router';
 import _ from 'lodash';
+import BackgroundImage from 'components/background_image';
+
+/**
+ * Artist item module
+ * @module artist/artist_item
+ */
 
 class ArtistItem extends React.Component {
 
   render() {
 
     let artist = this.props.artist,
-        name = _.trunc(artist.name,40),
-        imageUrl = (!_.isEmpty(artist.images)) ? artist.images[1].url : '',
-        bgImage = {
-          backgroundImage: 'url(' + imageUrl + ')',
-        };
+        name = _.trunc(artist.name,40);
 
     return (
       <div className="album-item">
         <div className="thumbnail">
           <div className="album-item-image-wrapper">
-            <div className="album-item-image-container" style={bgImage} />
+            <BackgroundImage image={artist.images[1]} classNames="album-item-image-container" />
           </div>
           <Link className="caption" to="artist" params={{ id: artist.id }}>{name}</Link>
         </div>
